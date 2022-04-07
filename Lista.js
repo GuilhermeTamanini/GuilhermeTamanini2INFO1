0dd91749f91b7f85e15f528e3dc4fcba655ebd30
Felipe Rotermel <feliperotermell@gmail.com>
2022-04-07 10:59:43 -0300

    commit

---
+++ Lista.js
@@ -0,0 +1,31 @@
+let listaCompras = [
+    {id:0, removido:false, item:'Pera'},
+    {id:1, removido:false, item:'Pera'},
+    {id:2, removido:false, item:'Pera'},
+    {id:3, removido:false, item:'Pera'},
+    {id:4, removido:false, item:'Pera'},
+    {id:5, removido:false, item:'Pera'},
+]
+
+
+
+
+
+
+
+
+function alternarItem(item){
+    const valor = listaCompras
+}
+
+function addItemUl(item) {
+    const inputItem = document.createElement('input')
+    inputItem.setAttribute('type', 'checkbox')
+    inputItem.addEventListener('click',alternarItem(item))
+
+    const labelItem 
+
+}
+function renderListaDeCompras {
+
+}
